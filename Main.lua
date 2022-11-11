2c34368732fdc6d51abc1354872d1a2061cfa1df
lolconnect <117947174+lolconnect@users.noreply.github.com>
2022-11-11 23:03:48 +0300

    Main.lua

---
+++ Main.lua
@@ -0,0 +1,14 @@
+local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+
+-- Creates an interactable prompt
+Inviter.Prompt({
+    name = "Test bozo",
+    invite = "your invite",
+})
+
+
+----- ALTERNATIVELY -----
+
+
+-- Will skip the prompt and invite the user right away
+Inviter.Join("your invite")
